6bffc5d81957ee9b5de70f363741783168b7ffc5
Igor Chepurnoy <igorzfort@gmail.com>
2015-03-10 19:12:45 +0200

    Update _form.php

--- views/cms/_form.php
+++ views/cms/_form.php
@@ -14,30 +14,31 @@ use \yii2mod\cms\models\enumerables\CmsStatus;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?php echo $form->field($model, 'title')->textInput(['maxlength' => 255]) ?>
+    <?php echo $form->field($model, 'title')->textInput(['maxlength' => 255]); ?>
 
     <?php echo $form->field($model, 'content')->widget(Widget::className(), [
         'options' => [
             'minHeight' => 200,
-        ]
-    ])
+        ],
+        'id' => 'content'
+    ]);
     ?>
 
     <?php echo $form->field($model, 'url', [
         'inputTemplate' => '<div class="input-group"><span class="input-group-addon">' . Yii::$app->urlManager->hostInfo . '/' . '</span>{input}</div>',
     ])->textInput(['maxlength' => 255])->hint('This one accepts only letters, numbers, dash and slash, i.e. "docs/installation".'); ?>
 
-    <?php echo $form->field($model, 'metaTitle')->textInput(['maxlength' => 255]) ?>
+    <?php echo $form->field($model, 'metaTitle')->textInput(['maxlength' => 255]); ?>
 
-    <?php echo $form->field($model, 'metaDescription')->textarea(['rows' => 6]) ?>
+    <?php echo $form->field($model, 'metaDescription')->textarea(['rows' => 6]); ?>
 
-    <?php echo $form->field($model, 'metaKeywords')->textarea(['rows' => 6]) ?>
+    <?php echo $form->field($model, 'metaKeywords')->textarea(['rows' => 6]); ?>
 
-    <?php echo $form->field($model, 'status')->dropDownList(CmsStatus::$list) ?>
+    <?php echo $form->field($model, 'status')->dropDownList(CmsStatus::$list); ?>
 
     <div class="form-group">
-        <?php echo Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-        <?php echo Html::a(Yii::t('app', 'Cancel'), ['index'], ['class' => 'btn btn-default']) ?>
+        <?php echo Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']); ?>
+        <?php echo Html::a(Yii::t('app', 'Cancel'), ['index'], ['class' => 'btn btn-default']); ?>
     </div>
 
     <?php ActiveForm::end(); ?>
