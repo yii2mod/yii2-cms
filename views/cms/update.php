3bc235437e3e54f8d4a43c5b7c947bdb1184cab6
sergeybochkarev <sergey.bochkarev@starcode.ru>
2015-09-21 17:51:24 +0300

    Use yii2mod

--- views/cms/update.php
+++ views/cms/update.php
@@ -4,11 +4,11 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model yii2mod\cms\models\CmsModel */
 
-$this->title = Yii::t('modcms', 'Update Page: ', [
+$this->title = Yii::t('yii2mod', 'Update Page: ', [
     'modelClass' => 'Cms Model',
 ]) . ' ' . $model->title;
-$this->params['breadcrumbs'][] = ['label' => Yii::t('modcms', 'Cms Pages'), 'url' => ['index']];
-$this->params['breadcrumbs'][] = Yii::t('modcms', 'Update');
+$this->params['breadcrumbs'][] = ['label' => Yii::t('yii2mod', 'Cms Pages'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = Yii::t('yii2mod', 'Update');
 ?>
 <div class="cms-model-update">
 
