cf76b4fad76823d80b9ec6e1ed216514c36ea146
sergeybochkarev <sergey.bochkarev@starcode.ru>
2015-09-21 16:35:12 +0300

    Move message to individual language file

--- views/cms/create.php
+++ views/cms/create.php
@@ -6,10 +6,10 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model yii2mod\cms\models\CmsModel */
 
-$this->title = Yii::t('app', 'Create Page', [
+$this->title = Yii::t('modcms', 'Create Page', [
     'modelClass' => 'Cms Model',
 ]);
-$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Cms Pages'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => Yii::t('modcms', 'Cms Pages'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="cms-model-create">
