e6202de405441d2e7776081d80aeccf52752a93f
sergeybochkarev <sergey.bochkarev@starcode.ru>
2015-09-21 17:49:00 +0300

    Use yii2mod

--- views/cms/create.php
+++ views/cms/create.php
@@ -6,10 +6,10 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model yii2mod\cms\models\CmsModel */
 
-$this->title = Yii::t('modcms', 'Create Page', [
+$this->title = Yii::t('yii2mod', 'Create Page', [
     'modelClass' => 'Cms Model',
 ]);
-$this->params['breadcrumbs'][] = ['label' => Yii::t('modcms', 'Cms Pages'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => Yii::t('yii2mod', 'Cms Pages'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="cms-model-create">
