6e3bf223c5e7ac7fd806c4f6086297b8205afc5f
sergeybochkarev <sergey.bochkarev@starcode.ru>
2015-09-21 16:34:44 +0300

    Add language message support
    and move existing message to individual file

--- views/cms/_form.php
+++ views/cms/_form.php
@@ -65,7 +65,7 @@ use \yii2mod\cms\models\enumerables\CmsStatus;
 
     <?php echo $form->field($model, 'url', [
         'inputTemplate' => '<div class="input-group"><span class="input-group-addon">' . Yii::$app->urlManager->hostInfo . '/' . '</span>{input}</div>',
-    ])->textInput(['maxlength' => 255])->hint('This one accepts only letters, numbers, dash and slash, i.e. "docs/installation".'); ?>
+    ])->textInput(['maxlength' => 255])->hint(Yii::t('modcms', 'This one accepts only letters, numbers, dash and slash, i.e. "docs/installation".')); ?>
 
     <?php echo $form->field($model, 'metaTitle')->textInput(['maxlength' => 255]); ?>
 
@@ -76,10 +76,10 @@ use \yii2mod\cms\models\enumerables\CmsStatus;
     <?php echo $form->field($model, 'status')->dropDownList(CmsStatus::$list); ?>
 
     <div class="form-group">
-        <?php echo Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']); ?>
-        <?php echo Html::a(Yii::t('app', 'Cancel'), ['index'], ['class' => 'btn btn-default']); ?>
+        <?php echo Html::submitButton($model->isNewRecord ? Yii::t('modcms', 'Create') : Yii::t('modcms', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']); ?>
+        <?php echo Html::a(Yii::t('modcms', 'Cancel'), ['index'], ['class' => 'btn btn-default']); ?>
         <?php if (!$model->isNewRecord) : ?>
-            <?php echo Html::a('Revert Content To Default', ['revert', 'id' => $model->id], ['class' => 'btn btn-primary']); ?>
+            <?php echo Html::a(Yii::t('modcms', 'Revert Content To Default'), ['revert', 'id' => $model->id], ['class' => 'btn btn-primary']); ?>
         <?php endif; ?>
     </div>
 
