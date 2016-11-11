0ab0f7bf69c5200a7222bc1f1347b00adedcd655
igor-chepurnoi <igorzfort@gmail.com>
2016-11-11 10:41:04 +0200

    fix issue 'table already exists' for windows

--- migrations/m161109_105445_rename_cms_table.php
+++ migrations/m161109_105445_rename_cms_table.php
@@ -6,22 +6,15 @@ class m161109_105445_rename_cms_table extends Migration
 {
     public function up()
     {
-        $this->renameTable('{{%Cms}}', '{{%cms}}');
+        if (Yii::$app->db->schema->getTableSchema('cms') === null) {
+            $this->renameTable('{{%Cms}}', '{{%cms}}');
+        }
     }
 
     public function down()
     {
-        $this->renameTable('{{%cms}}', '{{%Cms}}');
+        if (Yii::$app->db->schema->getTableSchema('Cms') === null) {
+            $this->renameTable('{{%cms}}', '{{%Cms}}');
+        }
     }
-
-    /*
-    // Use safeUp/safeDown to run migration code within a transaction
-    public function safeUp()
-    {
-    }
-
-    public function safeDown()
-    {
-    }
-    */
 }
